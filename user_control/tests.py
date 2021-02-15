2d48a9b4ba597a6ef329558e3f0641f51a3950ea
zahirekrem09 <1820980144@stu.adu.edu.tr>
2021-02-15 18:17:01 +0200

    added auth test

--- user_control/tests.py
+++ user_control/tests.py
@@ -27,10 +27,76 @@ class TestGenericFunctions(APITestCase):
 
         # check that we obtained a result
         self.assertTrue(token)
-        
+
     def test_get_refresh_token(self):
 
         token = get_refresh_token()
 
         # check that we obtained a result
         self.assertTrue(token)
+
+
+class TestAuth(APITestCase):
+    login_url = "/user/login"
+    register_url = "/user/register"
+    refresh_url = "/user/refresh"
+
+    def test_register(self):
+        payload = {
+            "username": "ekrem12",
+            "password": "ekrm123",
+            "email": "ekrem12@yahoo.com"
+        }
+
+        response = self.client.post(self.register_url, data=payload)
+
+        # check that we obtain a status of 201
+        self.assertEqual(response.status_code, 201)
+
+    def test_login(self):
+        payload = {
+            "username": "ekrem12",
+            "password": "ekrm123",
+            "email": "ekrem12@yahoo.com"
+        }
+
+        # register
+        self.client.post(self.register_url, data=payload)
+
+        # login
+        response = self.client.post(self.login_url, data=payload)
+        result = response.json()
+        # print(result)
+
+        # check that we obtain a status of 200
+        self.assertEqual(response.status_code, 200)
+
+        # check that we obtained both the refresh and access token
+        self.assertTrue(result["access"])
+        self.assertTrue(result["refresh"])
+
+    def test_refresh(self):
+        payload = {
+            "username": "ekrem12",
+            "password": "ekrm123",
+            "email": "ekrem12@yahoo.com"
+        }
+
+        # register
+        self.client.post(self.register_url, data=payload)
+
+        # login
+        response = self.client.post(self.login_url, data=payload)
+        refresh = response.json()["refresh"]
+
+        # get refresh
+        response = self.client.post(
+            self.refresh_url, data={"refresh": refresh})
+        result = response.json()
+
+        # check that we obtain a status of 200
+        self.assertEqual(response.status_code, 200)
+
+        # check that we obtained both the refresh and access token
+        self.assertTrue(result["access"])
+        self.assertTrue(result["refresh"])
