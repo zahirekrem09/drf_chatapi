9b12f26b5674fe45e6c594c6a8313ce83bc0f5ed
zahirekrem09 <1820980144@stu.adu.edu.tr>
2021-02-16 12:48:20 +0200

    added users filter

--- user_control/views.py
+++ user_control/views.py
@@ -11,6 +11,8 @@ from django.contrib.auth import authenticate
 from rest_framework.response import Response
 from .authentication import Authentication
 from rest_framework.permissions import IsAuthenticated
+import re
+from django.db.models import Q, Count, Subquery, OuterRef
 
 
 def get_random(length):
@@ -100,3 +102,38 @@ class UserProfileView(ModelViewSet):
     queryset = UserProfile.objects.all()
     serializer_class = UserProfileSerializer
     permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        data = self.request.query_params.dict()
+        keyword = data.get('keyword', None)
+        if keyword:
+            search_fields = (
+                'user__username', 'first_name', 'last_name',
+            )
+            query = self.get_query(keyword, search_fields)
+
+            return self.queryset.filter(query).distinct()
+
+        return self.queryset
+
+    @staticmethod
+    def get_query(q_string, search_fields):
+        query = None  # Query to search for every search term
+        terms = UserProfileView.normalize_query(q_string)
+        for term in terms:
+            or_query = None  # Query to search for a given term in each field
+            for field_name in search_fields:
+                q = Q(**{"%s__icontains" % field_name: term})
+                if or_query is None:
+                    or_query = q
+                else:
+                    or_query = or_query | q
+            if query is None:
+                query = or_query
+            else:
+                query = query & or_query
+        return query
+
+    @staticmethod
+    def normalize_query(query_string, findterms=re.compile(r'"([^"]+)"|(\S+)').findall, normspace=re.compile(r'\s{2,}').sub):
+        return [normspace(' ', (t[0] or t[1]).strip()) for t in findterms(query_string)]
