56400690b10a0a517154fd73b0e81ea692ea7859
zahirekrem09 <1820980144@stu.adu.edu.tr>
2021-02-16 14:36:23 +0200

    fixed bugs

--- chatapi/settings.py
+++ chatapi/settings.py
@@ -32,6 +32,13 @@ ALLOWED_HOSTS = ['*']
 AUTH_USER_MODEL = "user_control.CustomUser"
 
 
+REST_FRAMEWORK = {
+    # 'EXCEPTION_HANDLER': 'chatapi.custom_methods.custom_exception_handler',
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 20,
+}
+
+
 # Application definition
 
 INSTALLED_APPS = [
--- user_control/serializers.py
+++ user_control/serializers.py
@@ -31,23 +31,23 @@ class UserProfileSerializer(serializers.ModelSerializer):
     profile_picture = GenericFileUploadSerializer(read_only=True)
     profile_picture_id = serializers.IntegerField(
         write_only=True, required=False)
-    # message_count = serializers.SerializerMethodField("get_message_count")
+    message_count = serializers.SerializerMethodField("get_message_count")
 
     class Meta:
         model = UserProfile
         fields = "__all__"
 
-    # def get_message_count(self, obj):
-    #     try:
-    #         user_id = self.context["request"].user.id
-    #     except Exception as e:
-    #         user_id = None
+    def get_message_count(self, obj):
+        try:
+            user_id = self.context["request"].user.id
+        except Exception as e:
+            user_id = None
 
-    #     from message_control.models import Message
-    #     message = Message.objects.filter(
-    #         sender_id=obj.user.id, receiver_id=user_id, is_read=False).distinct()
+        from message_control.models import Message
+        message = Message.objects.filter(
+            sender_id=obj.user.id, receiver_id=user_id, is_read=False).distinct()
 
-    #     return message.count()
+        return message.count()
 
 
 # class FavoriteSerializer(serializers.Serializer):
